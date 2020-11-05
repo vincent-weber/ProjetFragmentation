ad18a1f5118ad5eb974c3518af371762bbcad1e8
Vincent <vincent.weber@etu.univ-amu.fr>
2020-11-05 12:43:33 +0100

    tetraedre.h corrige

--- starterLight/tetraedre.h
+++ starterLight/tetraedre.h
@@ -9,6 +9,13 @@ class Tetraedre
 public:
     std::array<Point*, 4> points;
     std::array<Tetraedre*, 4> tetra_adj;
+
+    //Pour respecter critere boule vide
+    Point circumsphere_center;
+
+    //Pour l'algorithme walk il le faut apparemment
+    Point insphere_center;
+
     Tetraedre(Point* p1, Point* p2, Point* p3, Point* p4);
     Tetraedre();
 };
